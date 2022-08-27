d00c34396823093be7e3de9d611261a210cf245e
Matheus <matheusbrito174@gmail.com>
2022-08-27 11:10:00 -0300

    Exibindo componentes no header e footer dinamicamente

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(readonly router: Router, private readonly _location: Location) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goBack() {
+    this._location.back();
   }
-
 }
